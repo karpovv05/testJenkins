b4dadb10884d2427839f0d23c4939137185a6c47
karpovv05 <karps5599@icloud.com>
2025-06-01 23:56:44 +0300

    1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ dp = Dispatcher(bot)
 @dp.message_handler()
 async def echo(message):
     await message.answer(message.text)
-    logging.info(f"Пришло сообщение {message.textß}")
+    logging.info(f"Пришло сообщение {message.text}")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
